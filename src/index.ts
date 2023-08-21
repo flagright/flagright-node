a073e9682779116b546e3400c2e7c745a81c5052
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2023-08-21 14:13:18 +0000

    Release 1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export * as Flagright from "./api";
 export { FlagrightClient } from "./Client";
-export { FlagrightEnvironment } from "./environments";
 export { FlagrightError, FlagrightTimeoutError } from "./errors";
