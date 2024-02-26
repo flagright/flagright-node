c8f42f7610488bc4e7ef4545d882c48628d5db5c
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-02-26 14:23:27 +0000

    Release 1.4.4

--- src/api/resources/consumerUserEvents/client/Client.ts
+++ src/api/resources/consumerUserEvents/client/Client.ts
@@ -188,7 +188,7 @@ export class ConsumerUserEvents {
                 "x-api-key": await core.Supplier.get(this._options.apiKey),
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "flagright",
-                "X-Fern-SDK-Version": "1.4.3",
+                "X-Fern-SDK-Version": "1.4.4",
             },
             contentType: "application/json",
             queryParameters: _queryParams,
