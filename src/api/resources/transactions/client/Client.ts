abc0f799b5d920b56c9cea1e9f5572d39d44ba17
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-03-05 07:27:41 +0000

    Release 1.6.48

--- src/api/resources/transactions/client/Client.ts
+++ src/api/resources/transactions/client/Client.ts
@@ -37,7 +37,7 @@ export class Transactions {
      * Here are some of the most used payload fields explained (you can find the full payload [schema below](/api-reference/api-reference/transactions/verify#request) with 1 line descriptions):
      *
      * * `type`: Type of transaction (Ex: `WITHDRAWAL`, `DEPOSIT`, `TRANSFER` etc).
-     * * `transactionId` - Unique Identifier for the transaction. Flagright API will generate a `transactionId` if this field is left empty
+     * * `transactionId` - Unique Identifier for the transaction.
      * * `timestamp` - UNIX timestamp in *milliseconds* of when the transaction took place
      * * `transactionState` - The state of the transaction, set to `CREATED` by default. [More details here](/guides/overview/entities#transaction-lifecycle-through-transaction-events)
      * * `originUserId` - Unique identifier (if any) of the user who is sending the money. This user must be created within the Flagright system before using the [create a consumer user](/api-reference/api-reference/consumer-users/create) or [create a business user](/api-reference/api-reference/business-users/create) endpoint
@@ -148,7 +148,7 @@ export class Transactions {
                 "x-api-key": await core.Supplier.get(this._options.apiKey),
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "flagright",
-                "X-Fern-SDK-Version": "1.6.47",
+                "X-Fern-SDK-Version": "1.6.48",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
             },
@@ -247,7 +247,7 @@ export class Transactions {
                 "x-api-key": await core.Supplier.get(this._options.apiKey),
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "flagright",
-                "X-Fern-SDK-Version": "1.6.47",
+                "X-Fern-SDK-Version": "1.6.48",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
             },
